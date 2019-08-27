15f918486c086c75fdb0743548f511f88044ebd3
sieder <sieder00@gmail.com>
2019-08-27 15:05:27 +0300

    adding files for github pages

---
+++ build/precache-manifest.ec75f7c5429cf8e2dbac0810f3c087e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e68adb080f3790ca9972653f9299c3",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "1440a385168ae35f9f4b",
+    "url": "/monsters/static/css/main.baad3ebd.chunk.css"
+  },
+  {
+    "revision": "186e125adad4e8e5f83a",
+    "url": "/monsters/static/js/2.d7de8aea.chunk.js"
+  },
+  {
+    "revision": "1440a385168ae35f9f4b",
+    "url": "/monsters/static/js/main.0a3dc501.chunk.js"
+  },
+  {
+    "revision": "49843bb592482364dc67",
+    "url": "/monsters/static/js/runtime~main.087b14e1.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9da11ca5de7f9bca74071dfc841004ee.js"
+  "/monsters/precache-manifest.ec75f7c5429cf8e2dbac0810f3c087e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
